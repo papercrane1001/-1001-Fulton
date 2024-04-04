3d5f5df1fd10a781c767732b88a3c2bd95ed1bf8
papercrane1001 <papercrane1001@gmail.com>
2024-04-04 15:15:12 -0700

    I've gone too deep

---
+++ 1.4/Source/FultonDevice/FultonDevice/FultonDeviceBelt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace FultonDevice
+{
+    public class FultonDeviceBelt : Apparel
+    {
+
+        public override IEnumerable<Gizmo> GetWornGizmos()
+        {
+            IEnumerable<Gizmo> gizmos = base.GetWornGizmos();
+            FultonAbility fa = new FultonAbility();
+            if(this.Wearer != null) { fa.pawn = this.Wearer; }
+            gizmos.Append(new Command_Ability(fa)
+            {
+                defaultLabel = "defaultLabel",
+                defaultDesc = "defaultDesc"
+            }) ;
+            return gizmos;
+        }
+    }
+}
---
+++ 1.4/Source/FultonDevice/FultonDevice/Verb_Fulton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace FultonDevice
+{
+    public class Verb_Fulton : Verb
+    {
+        protected override bool TryCastShot()
+        {
+            //throw new NotImplementedException();
+            return true;
+        }
+
+        protected override float EffectiveRange
+        {
+            get
+            {
+                return 1.2f;
+            }
+        }
+
+
+
+    }
+}
