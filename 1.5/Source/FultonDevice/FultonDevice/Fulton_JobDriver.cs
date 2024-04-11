f298edf03d0d452dd37624272355b14dfff850f0
papercrane1001 <papercrane1001@gmail.com>
2024-04-11 12:54:14 -0700

    1.5 rename

---
+++ 1.5/Source/FultonDevice/FultonDevice/Fulton_JobDriver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse.AI;
+
+namespace FultonDevice
+{
+    public class Fulton_JobDriver : JobDriver
+    {
+        public override bool TryMakePreToilReservations(bool errorOnFailed)
+        {
+            return pawn.Reserve(job.targetA, job, 1, -1, null, errorOnFailed);
+        }
+        public override IEnumerable<Toil> MakeNewToils()
+        {
+
+        }
+    }
+}
